5c33291932ee24e03cbdc6c385ea6cdc2cf1daae
Lydian Lee <lydian@cmu.edu>
2013-06-06 10:43:09 -0700

    add ot operation

--- routes/codeMirror.js
+++ routes/codeMirror.js
@@ -3,11 +3,14 @@ var ot = require("ot");
 var Doc = function(sessionId){
   this.sessionId = sessionId;
   this.server = new ot.Server("");
-  this.onReceive = function(json){
-    
+  this.onReceive = function(data){
+    data.operation = ot.TextOperation(data.strOperation);
+    this.server.receiveOperation(data.revision, data.operation);
+    this.broadcast();
   }
   this.broadcast = function(){
     //Todo: push data to MongoDB
+
   }
 }
 var docs = {};
@@ -18,7 +21,7 @@ exports.editor = function(req, res){
   if(docs[sessionId] === undefined ) {
     docs[sessionId] = new Doc();
   }
-
+  docs[sessionId].onReceive(json);
   //broadcast 
   return res.send(docs);
   //return res.render("index", {"title": "editor"});
